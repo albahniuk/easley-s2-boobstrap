fd0666b02c372ebac667bddbbd4b1d9239cec5c0
lilia <liliaborrazas14@gmail.com>
2018-11-28 19:34:59 +0100

    firt event

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,10 @@
 'use strict';
-console.log('quiero que arranque');
-alert('hola');
+const nameField = document.querySelector('#name');
+const nameCard = document.querySelector('.h1-description');
+nameField.addEventListener('keyup', function(e){
+
+  const autor = e.currentTarget;
+
+  nameCard.innerHTML = autor.value;
+
+});

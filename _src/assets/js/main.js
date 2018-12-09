e9495c0a5f2da350f73528bbf242130837c9f063
mariaoca <mariaolidcamarena@gmail.com>
2018-12-09 16:17:05 +0100

    finish medialinks

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -48,16 +48,26 @@ fileField.addEventListener('change', getImage);
 //Links
 const phoneOrigin = document.querySelector ('#phone');
 const phoneDest = document.querySelector ('.contact-list__tlf');
+const mailOrigin = document.querySelector ('#email');
+const mailDest = document.querySelector ('.contact-list__mail');
+const linkOrigin = document.querySelector ('#linkedin');
+const linkDest = document.querySelector ('.contact-list__linkedin');
+const gitOrigin = document.querySelector ('#github');
+const gitDest = document.querySelector('.contact-list__github');
 
 function writeMe(e) {
-  const tel = e.currentTarget.value;
+  const author = e.currentTarget.value;
   const destCommon = e.currentTarget.getAttribute('data-common');
   const destIco = e.currentTarget.getAttribute('data-ico');
   const dataDest = e.currentTarget.getAttribute('data-dest');
-  document.querySelector(dataDest).innerHTML = `<a href="${destCommon}${tel}"><i class="${destIco}"></i></a>`;
+  document.querySelector(dataDest).innerHTML = `<a href="${destCommon}${author}"><i class="${destIco}"></i></a>`;
 }
 
 phoneOrigin.addEventListener('keyup', writeMe);
+mailOrigin.addEventListener('keyup', writeMe);
+linkOrigin.addEventListener('keyup', writeMe);
+gitOrigin.addEventListener('keyup', writeMe);
+
 
 
 

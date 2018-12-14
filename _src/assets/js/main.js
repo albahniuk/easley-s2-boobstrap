76cc44b613116ce956fbabad49cc39c2057fd577
albahniuk <albalopezfolgar@gmail.com>
2018-12-14 16:32:57 +0100

    collapsible

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -293,3 +293,25 @@ function clean() {
 }
 
 reset.addEventListener('click',clean);
+
+
+//collapsible
+
+const buttonCollapsible = document.querySelectorAll('.collapsible__clickable');
+const designContent = document.querySelector('.collapsible__content-design');
+const fillContent = document.querySelector('.collapsible__content-fill');
+const shareContent = document.querySelector('.collapsible__content-share');
+
+function collapsible(event) {
+  const clickableSection = event.currentTarget;
+  if (clickableSection.classList.contains('collapsible__design') === true) {
+    designContent.classList.toggle('hidden');
+  } else if (clickableSection.classList.contains('collapsible__fill') === true) {
+    fillContent.classList.toggle('hidden');
+  } else {
+    shareContent.classList.toggle('hidden');
+  }
+}
+for (let i = 0; i < buttonCollapsible.length; i++) {
+  buttonCollapsible[i].addEventListener('click', collapsible);
+}
